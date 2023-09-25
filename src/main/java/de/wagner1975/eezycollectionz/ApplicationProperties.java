5e55863e8058be51c01a79088e154a9822ab9a44
Lars Wagner <wagner1975@web.de>
2023-09-25 23:20:05 +0200

    Renamed application properties

--- src/main/java/de/wagner1975/eezycollectionz/ApplicationProperties.java
+++ src/main/java/de/wagner1975/eezycollectionz/ApplicationProperties.java
@@ -2,7 +2,7 @@ package de.wagner1975.eezycollectionz;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(value = "stubben")
+@ConfigurationProperties(value = "eezycollectionz")
 public record ApplicationProperties(
   String welcomeMessage,
   String author
