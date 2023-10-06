3e2fc142729d48b8f35a50eaff748b36741fa47a
Lars Wagner <wagner1975@web.de>
2023-10-06 08:10:06 +0200

    Added tests to collection controller test

--- src/test/java/de/wagner1975/eezycollectionz/collection/CollectionControllerTest.java
+++ src/test/java/de/wagner1975/eezycollectionz/collection/CollectionControllerTest.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(CollectionController.class)
@@ -30,7 +31,7 @@ class CollectionControllerTest {
   private CollectionService mockService;
 
   @Test
-  void shouldCreateWhenPayloadValid() throws Exception {
+  void post_Success_IsCreated() throws Exception {
       var collectionInput = CollectionInput.builder().name("xyz").build(); 
     
       when(mockService.create(any()))
@@ -44,5 +45,39 @@ class CollectionControllerTest {
           .contentType(MediaType.APPLICATION_JSON)
           .content(objectMapper.writeValueAsString(collectionInput)))
         .andExpect(MockMvcResultMatchers.status().isCreated());
+  }
+
+  @Test
+  void post_FailureOnIdGeneration_UnprocessableEntity() throws Exception {
+      when(mockService.create(any())).thenReturn(Optional.empty());
+
+      mockMvc
+        .perform(post("/api/collections")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(objectMapper.writeValueAsString(CollectionInput.builder().name("xyz").build())))
+        .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
+  }
+
+  @Test
+  void post_PayloadInvalid_BadRequest() throws Exception {
+      mockMvc
+        .perform(post("/api/collections")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(objectMapper.writeValueAsString(CollectionInput.builder().name("").build())))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest());    
+  }
+
+  @Test
+  void delete_Success_NoContent() throws Exception {
+      mockMvc
+        .perform(delete("/api/collections/c725efeb-de77-46df-916a-2fc195376386"))
+        .andExpect(MockMvcResultMatchers.status().isNoContent());
+  }
+
+  @Test
+  void delete_IdInvalid_BadRequest() throws Exception {
+      mockMvc
+        .perform(delete("/api/collections/1gg1"))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest());    
   }
 }
