a03c5955fdd55829a349acf937e7d128aacd994e
Lars Wagner <wagner1975@web.de>
2023-11-22 05:57:55 +0100

    Minor modifications on integration test

--- src/test/java/de/wagner1975/eezycollectionz/collection/CollectionControllerIntegTest.java
+++ src/test/java/de/wagner1975/eezycollectionz/collection/CollectionControllerIntegTest.java
@@ -46,37 +46,42 @@ class CollectionControllerIntegTest {
   @BeforeEach
   void setUp() {
     RestAssured.baseURI = "http://localhost:" + port;
+  
     repository.deleteAll();
+
+    var collections = List.of(
+      createCollection("00000000-0000-aabb-0000-000000000000", "Liste Z"),
+      createCollection("00000001-1111-bbcc-0000-000000000001", "Liste Y"),
+      createCollection("00000002-2222-ccdd-0000-000000000002", "Liste X"),
+      createCollection("00000003-3333-ddee-0000-000000000003", "Liste W"),
+      createCollection("00000004-4444-eeff-0000-000000000004", "Liste V"),
+      createCollection("00000005-5555-ffee-0000-000000000005", "Liste U"),
+      createCollection("00000006-6666-eedd-0000-000000000006", "Liste T"),
+      createCollection("00000007-7777-ddcc-0000-000000000007", "Liste S"),
+      createCollection("00000008-8888-ccbb-0000-000000000008", "Liste R"),
+      createCollection("00000009-9999-bbaa-0000-000000000009", "Liste Q"));
+
+    repository.saveAll(collections);
   }
 
   @Test
   void get_Success_Ok() {
-    var now = Instant.now();
-    var id1 = "00000001-1111-aaaa-0000-000000000001";
-    var id2 = "00000002-2222-bbbb-0000-000000000002";    
-
-    var collections = List.of(
-      Collection.builder()
-        .id(UUID.fromString(id1))
-        .createdAt(now)
-        .lastModifiedAt(now)
-        .name("Liste A")
-        .build(),
-      Collection.builder()
-        .id(UUID.fromString(id2))
-        .createdAt(now)
-        .lastModifiedAt(now)
-        .name("Liste B")
-        .build());        
-
-    repository.saveAll(collections);
-
     given()
       .contentType(ContentType.JSON)
       .when()
       .get("/api/collections")
       .then()
       .statusCode(200)
-      .body("content", hasSize(2));
-  }  
+      .body("content", hasSize(10));
+  }
+
+  private Collection createCollection(String id, String name) {
+    var now = Instant.now();
+    return Collection.builder()
+      .id(UUID.fromString(id))
+      .createdAt(now)
+      .lastModifiedAt(now)
+      .name(name)
+      .build();    
+  }
 }
