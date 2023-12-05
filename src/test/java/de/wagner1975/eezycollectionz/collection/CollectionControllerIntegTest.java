c445704212513532a27ce63b8a2aa573d9b0e344
Lars Wagner <wagner1975@web.de>
2023-12-06 00:35:05 +0100

    Assert that createdAt contains current time

--- src/test/java/de/wagner1975/eezycollectionz/collection/CollectionControllerIntegTest.java
+++ src/test/java/de/wagner1975/eezycollectionz/collection/CollectionControllerIntegTest.java
@@ -4,9 +4,11 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.matchesPattern;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,7 +28,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -60,26 +61,26 @@ class CollectionControllerIntegTest {
     repository.deleteAll();
 
     var collections = List.of(
-      createCollection("00000000-0000-aabb-0000-000000000000", "Collection Z"),
-      createCollection("00000001-1111-bbcc-0000-000000000001", "Collection Y"),
-      createCollection("00000002-2222-ccdd-0000-000000000002", "Collection X"),
-      createCollection("00000003-3333-ddee-0000-000000000003", "Collection W"),
-      createCollection("00000004-4444-eeff-0000-000000000004", "Collection V"),
-      createCollection("00000005-5555-ffee-0000-000000000005", "Collection U"),
-      createCollection("00000006-6666-eedd-0000-000000000006", "Collection T"),
-      createCollection("00000007-7777-ddcc-0000-000000000007", "Collection S"),
-      createCollection("00000008-8888-ccbb-0000-000000000008", "Collection R"),
-      createCollection("00000009-9999-bbaa-0000-000000000009", "Collection Q"),
-      createCollection("10000000-0000-aabb-0000-000000000000", "Collection P"),
-      createCollection("10000001-1111-bbcc-0000-000000000001", "Collection O"),
-      createCollection("10000002-2222-ccdd-0000-000000000002", "Collection N"),
-      createCollection("10000003-3333-ddee-0000-000000000003", "Collection M"),
-      createCollection("10000004-4444-eeff-0000-000000000004", "Collection L"),
-      createCollection("10000005-5555-ffee-0000-000000000005", "Collection K"),
-      createCollection("10000006-6666-eedd-0000-000000000006", "Collection J"),
-      createCollection("10000007-7777-ddcc-0000-000000000007", "Collection I"),
-      createCollection("10000008-8888-ccbb-0000-000000000008", "Collection H"),
-      createCollection("10000009-9999-bbaa-0000-000000000009", "Collection G"));      
+      createCollection("00000000-0000-4000-8000-aabb00000000", "Collection Z"),
+      createCollection("00000001-1111-4000-8000-bbcc00000001", "Collection Y"),
+      createCollection("00000002-2222-4000-8000-ccdd00000002", "Collection X"),
+      createCollection("00000003-3333-4000-8000-ddee00000003", "Collection W"),
+      createCollection("00000004-4444-4000-8000-eeff00000004", "Collection V"),
+      createCollection("00000005-5555-4000-8000-ffee00000005", "Collection U"),
+      createCollection("00000006-6666-4000-8000-eedd00000006", "Collection T"),
+      createCollection("00000007-7777-4000-8000-ddcc00000007", "Collection S"),
+      createCollection("00000008-8888-4000-8000-ccbb00000008", "Collection R"),
+      createCollection("00000009-9999-4000-8000-bbaa00000009", "Collection Q"),
+      createCollection("10000000-0000-4000-8000-aabb00000000", "Collection P"),
+      createCollection("10000001-1111-4000-8000-bbcc00000001", "Collection O"),
+      createCollection("10000002-2222-4000-8000-ccdd00000002", "Collection N"),
+      createCollection("10000003-3333-4000-8000-ddee00000003", "Collection M"),
+      createCollection("10000004-4444-4000-8000-eeff00000004", "Collection L"),
+      createCollection("10000005-5555-4000-8000-ffee00000005", "Collection K"),
+      createCollection("10000006-6666-4000-8000-eedd00000006", "Collection J"),
+      createCollection("10000007-7777-4000-8000-ddcc00000007", "Collection I"),
+      createCollection("10000008-8888-4000-8000-ccbb00000008", "Collection H"),
+      createCollection("10000009-9999-4000-8000-bbaa00000009", "Collection G"));      
 
     repository.saveAll(collections);
   }
@@ -97,11 +98,11 @@ class CollectionControllerIntegTest {
       statusCode(200).
       body(
         "content", hasSize(3),
-        "content[0].id", equalTo("10000003-3333-ddee-0000-000000000003"),
+        "content[0].id", equalTo("10000003-3333-4000-8000-ddee00000003"),
         "content[0].name", equalTo("Collection M"),
-        "content[1].id", equalTo("10000002-2222-ccdd-0000-000000000002"),
+        "content[1].id", equalTo("10000002-2222-4000-8000-ccdd00000002"),
         "content[1].name", equalTo("Collection N"),
-        "content[2].id", equalTo("10000001-1111-bbcc-0000-000000000001"),
+        "content[2].id", equalTo("10000001-1111-4000-8000-bbcc00000001"),
         "content[2].name", equalTo("Collection O"));      
   }
 
@@ -109,13 +110,13 @@ class CollectionControllerIntegTest {
   void getById_Success_Ok() {
     given().
       contentType(ContentType.JSON).
-      pathParam("id", "00000006-6666-eedd-0000-000000000006").
+      pathParam("id", "00000006-6666-4000-8000-eedd00000006").
     when().
       get(REQUEST_PATH + "/{id}").
     then().
       statusCode(200).
       body(
-        "id", equalTo("00000006-6666-eedd-0000-000000000006"),
+        "id", equalTo("00000006-6666-4000-8000-eedd00000006"),
         "name", equalTo("Collection T"));
   }  
 
@@ -123,10 +124,12 @@ class CollectionControllerIntegTest {
   @Transactional
   @Rollback
   void post_Success_Created() {
-    long countBefore = repository.count();
-    String newName = "A brand new collection";
+    var timeBefore = Instant.now().truncatedTo(ChronoUnit.MICROS);
+    var countBefore = repository.count();
+    
+    var newName = "A brand new collection";
 
-    Response response = 
+    var response = 
     given().
       contentType(ContentType.JSON).
       body(CollectionInput.builder().name(newName).build()).
@@ -141,14 +144,21 @@ class CollectionControllerIntegTest {
         "name", equalTo(newName)).
     extract().response();
 
-    long countAfter = countBefore + 1;
-    String newIdAsString = response.path("id");
+    var timeAfter = Instant.now().truncatedTo(ChronoUnit.MICROS); 
+    
+    var countAfter = countBefore + 1;
+    assertEquals(countAfter, repository.count());
+
     String createdAtAsString = response.path("createdAt");
     String lastModifiedAtAsString = response.path("lastModifiedAt");
-
-    assertEquals(countAfter, repository.count());
     assertEquals(createdAtAsString, lastModifiedAtAsString);
+
+    var createdAt = Instant.parse(createdAtAsString);
+    assertTrue(createdAt.compareTo(timeBefore) >= 0);
+    assertTrue(createdAt.compareTo(timeAfter) <= 0);
     
+    String newIdAsString = response.path("id");
+
     given().
       contentType(ContentType.JSON).
       pathParam("id", newIdAsString).
